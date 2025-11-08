00697d10568a849c6957a892dc2c13db1ce9f087
Yuki <455527653@qq.com>
2025-11-08 22:53:25 +0800

    add quote style

---
+++ blocks/quote/quote.js
@@ -0,0 +1,6 @@
+export default function decorate(block) {
+  const [quoteWrapper] = block.children;
+  const blockquote = document.createElement('blockquote');
+  blockquote.textContent = quoteWrapper.textContent.trim();
+  quoteWrapper.replaceChildren(blockquote);
+}
