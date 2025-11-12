12940c98a915092d3b2bd9f74700136bce9039ad
Yuki <455527653@qq.com>
2025-11-12 18:57:19 +0800

    1

--- blocks/featured-news-articles/featured-news-articles.js
+++ blocks/featured-news-articles/featured-news-articles.js
@@ -2,7 +2,6 @@ import { createOptimizedPicture } from "../../scripts/aem.js";
 import { moveInstrumentation } from "../../scripts/scripts.js";
 
 export default function decorate(block) {
-  return false;
   // 获取block中的所有div元素
   const divs = block.querySelectorAll(":scope > div");
 
@@ -27,7 +26,7 @@ export default function decorate(block) {
       });
     }
   }
-
+  return false;
   // 创建外层section
   const section = document.createElement("section");
   section.className = "section-with-bottom-spacing";
