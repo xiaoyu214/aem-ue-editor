b69df592fa16d783db0492aedc3ce1c487439ec3
Yuki <455527653@qq.com>
2025-11-14 17:19:04 +0800

    f

--- blocks/featured-news-articles/featured-news-articles.js
+++ blocks/featured-news-articles/featured-news-articles.js
@@ -1,25 +1,6 @@
 import { moveInstrumentation } from "../../scripts/scripts.js";
 import { isAuthorEnvironment, safeText } from "../../scripts/utils.js";
 export default async function decorate(block) {
-  const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    moveInstrumentation(row, li);
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
-    ul.append(li);
-  });
-  // ul.querySelectorAll('picture > img').forEach((img) => {
-  //   const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
-  //   moveInstrumentation(img, optimizedPic.querySelector('img'));
-  //   img.closest('picture').replaceWith(optimizedPic);
-  // });
-  block.textContent = '';
-  block.append(ul);
-  return false;
   const divs = block.children;
   const mockupContainer = document.createRange().createContextualFragment(`
         <div class="cmp-container container">
@@ -120,7 +101,9 @@ export default async function decorate(block) {
       );
     }
   }
-  block.replaceWith(mockupContainer);
+  // block.replaceWith(mockupContainer);
+  block.textContent = '';
+  block.append(mockupContainer);
 }
 
 function findFirstDataElement(element) {
