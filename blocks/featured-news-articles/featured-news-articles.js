faf90ba74861da3b883f975fd1736d58eb9d6071
Yuki <455527653@qq.com>
2025-11-18 17:56:20 +0800

    f

--- blocks/featured-news-articles/featured-news-articles.js
+++ blocks/featured-news-articles/featured-news-articles.js
@@ -2,21 +2,91 @@ import { transferInstrumentation } from "../../scripts/utils.js";
 import { isAuthorEnvironment } from "../../scripts/utils.js";
 export default async function decorate(block) {
   const divs = block.children;
-  const mockupContainer = document.createElement("div"); // 真实DOM容器
+  const mockupContainer = document.createRange().createContextualFragment(`
+        <div class="cmp-container container">
+          <div class="carousel panelcontainer">
+            <div class="section-heading">
+              <div class="section-heading__text-group">
+                <h2 class="section-heading__title">222</h2>
+              </div>
+              <div class="section-heading__action-buttons cmp-carousel__actions">
+                <button class="cmp-carousel__action cmp-carousel__action--previous">
+                  <span class="sr-only">Previous Button</span>
+                </button>
+                <button class="cmp-carousel__action cmp-carousel__action--next">
+                  <span class="sr-only">Previous Button</span>
+                </button>
+              </div>
+            </div>
+
+            <div class="cmp-carousel" role="group" aria-live="polite" aria-roledescription="carousel" data-cmp-is="carousel" data-cmp-delay="false" data-slides-per-view="auto" data-slides-per-view-tablet="3" data-slides-per-view-desktop="3" data-loop-slides="false">
+              <div class="cmp-carousel__content cmp-carousel__content--overflow"></div>
+            </div>
+          </div>
+        </div>
+        <div class="section-actions-container">
+          <a class="section-actions-btn btn btn-link">
+            333<img src="/content/dam/eds-enablement-xwalk/asus-cto-sites/icon-arrow.svg" alt="Arrow Right">
+            </a>
+        </div>`);
+        
+  const cardNodes = [];
+  for (let i = 0; i < divs.length; i++) {
+    const subDivs = divs[i].children;
+    const title = subDivs[0].textContent?.trim() || "";
+    const summary = subDivs[1].textContent?.trim() || "";
+    const image = subDivs[2].querySelector("img")?.getAttribute("src") || "";
+    const imageAlt = subDivs[3].textContent?.trim() || "";
+    const postedDate = subDivs[4].textContent?.trim();
+    const articleLink = subDivs[5].textContent?.trim();
+    const articleOpenInNewTab =
+      subDivs[6].textContent?.trim().toLowerCase() === "true";
+
+    const mockup = document.createRange().createContextualFragment(`
+      <div class="cmp-carousel__item">
+        <a class="cmp-article-card" href="${articleLink}" aria-label="${title}" target="${
+      articleOpenInNewTab ? "_blank" : "_self"
+    }">
+          <div class="cmp-article-card__image cmp-image">
+            <img class="cmp-image__image" src="${image}" alt="${imageAlt}" loading="lazy">
+          </div>
+
+          <div class="cmp-article-card__content">
+            <p class="cmp-article-card__date">
+              <time datetime="${postedDate}" aria-label="Date">
+                <span aria-hidden="true">
+                  ${postedDate}
+                </span>
+              </time>
+            </p>
+            <h3 class="cmp-article-card__title">${title}</h3>
+            <div class="cmp-article-card__desc">${summary}</div>
+          </div>
+        </a>
+      </div>
+    `);
+
+    //move card attr
+    if (isAuthorEnvironment()) {
+      transferInstrumentation(divs[i], mockup);
+    }
+
+    cardNodes.push(mockup);
+  }
+
+  mockupContainer.querySelector(".cmp-carousel__content").append(...cardNodes);
 
   //move attr
   if (isAuthorEnvironment()) {
     transferInstrumentation(block, mockupContainer);
 
-    if (divs[0]) {
-      const headline = document.createElement("h2");
-      headline.className = "section-heading__title";
-      headline.textContent = divs[0].textContent.trim();
-      mockupContainer.appendChild(headline);
-      // transferInstrumentation(divs[0], headline);
-    }
+    // if (divs[0]) {
+    //   transferInstrumentation(divs[0], mockupContainer.querySelector('.section-heading__title'));
+    // }
+    // if (divs[1]) {
+    //   transferInstrumentation(divs[1], mockupContainer.querySelector('.section-actions-container'));
+    // }
   }
 
-  block.textContent = '';
-  block.append(mockupContainer);
+  block.replaceWith(mockupContainer);
 }
