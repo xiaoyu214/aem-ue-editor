f7c179fc5331f6463aba5b6c8c317272c49458fe
Yuki <455527653@qq.com>
2025-11-19 14:36:21 +0800

    g

--- blocks/featured-news-articles/featured-news-articles.js
+++ blocks/featured-news-articles/featured-news-articles.js
@@ -2,10 +2,13 @@ import { transferInstrumentation } from "../../scripts/utils.js";
 import { isAuthorEnvironment } from "../../scripts/utils.js";
 export default async function decorate(block) {
   const divs = block.children;
-  const div = document.createElement('h2');
+  const div = document.createElement("h2");
   div.innerHTML = divs[0].textContent.trim();
-  div.className = 'section-heading__title';
+  div.className = "section-heading__title";
   block.replaceWith = div;
+  if (divs[0]) {
+    transferInstrumentation(divs[0], div);
+  }
   // const divs = block.children;
   // const mockupContainer = document.createRange().createContextualFragment(`
   //       <div class="cmp-container container">
